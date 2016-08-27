a2925159d4db58d667fce540afa9ea3f106379cf
Victoria French <victoria.french@cinecove.com>
2016-08-27 02:56:50 -0500

    new DialogReferenceManager

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,6 +20,7 @@
 * Updated React 15.2.1 to version 15.3.1
 * Updated React Native 0.31.0 to version 0.32.0
 * Updated PropTypes declaration to correct array
+* **NEW** DialogReferenceManager
 
 ### 0.6.0 (August 19, 2016)
 * Forked from react-native-popup
--- index.js
+++ index.js
@@ -12,6 +12,93 @@ import {
 	Platform,
 } from 'react-native';
 
+const _references = new WeakMap();
+
+class DialogReferenceManager {
+	constructor() {
+		_references.set(this, {});
+	}
+
+	register(name, component) {
+		if (component === void 0) {
+			throw new Error('A DialogBox component is required to register it with the registry');
+		}
+		if (typeof name === 'string' && name.length > 0) {
+			const storage = _references(this);
+			if (storage) {
+				if (storage[name] === void 0) {
+					storage[name] = component;
+					_references.set(this, storage);
+				} else {
+					throw new Error('A dialogbox with the name "' + name + '" was previously registered. Names must be unique and only one can act as "default"');
+				}
+			}
+		} else {
+			throw new Error('Attempt to register a dialogbox without a name occurred');
+		}
+	}
+
+	unregister(name) {
+		if (typeof name === 'string' && name.length > 0) {
+			const storage = _references(this);
+			if (storage && storage[name]) {
+				delete storage[name];
+				_references.set(this, storage);
+			}
+		} else {
+			throw new Error('Attempt to unregister a dialogbox without a name occurred');
+		}
+	}
+
+	dialogbox(name) {
+		const id = (typeof name === 'string' && name.length > 0) ? name : 'default';
+		const storage = _references(this);
+		if (storage && storage[id]) {
+			return storage[id];
+		}
+		return void 0;
+	}
+
+	alert(...text) {
+		const component = this.dialogbox('default');
+		if (component) {
+			component.alert(...text);
+		} else {
+			console.warn('An alert request to the default dialogbox was made, but no dialogbox was registered as default');
+		}
+	}
+
+	tip(args) {
+		const component = this.dialogbox('default');
+		if (component) {
+			component.tip(args);
+		} else {
+			console.warn('A tip request to the default dialogbox was made, but no dialogbox was registered as default');
+		}
+	}
+
+	confirm(args) {
+		const component = this.dialogbox('default');
+		if (component) {
+			component.confirm(args);
+		} else {
+			console.warn('A confirm request to the default dialogbox was made, but no dialogbox was registered as default');
+		}
+	}
+
+	close() {
+		const component = this.dialogbox('default');
+		if (component) {
+			component.close();
+		} else {
+			console.warn('A close request to the default dialogbox was made, but no dialogbox was registered as default');
+		}
+	}
+}
+
+const dialogReferenceManager = Object.freeze(new DialogReferenceManager());
+export { dialogReferenceManager as DialogReferenceManager }
+
 class PopContent extends Component{
 
 	static propTypes = {
@@ -145,6 +232,10 @@ export default class DialogBox extends Component{
 
 	static DisplayPopup = DisplayPopup;
 
+	static propTypes = {
+		name: PropTypes.string
+	};
+
 	static defaultProps = {
 		isOverlay: true,
 		isOverlayClickClose: true,
@@ -159,6 +250,29 @@ export default class DialogBox extends Component{
 			isOverlayClickClose: this.props.isOverlayClickClose,
 			content: null,
 		};
+	}
+
+	componentDidMount() {
+		if (this.props.name) {
+			dialogReferenceManager.register(this.props.name, this);
+		}
+	}
+
+	componentWillUnmount() {
+		if (this.props.name) {
+			dialogReferenceManager.unregister(this.props.name);
+		}
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (this.props.name !== nextProps.name) {
+			if (this.props.name) {
+				dialogReferenceManager.unregister(this.props.name);
+			}
+			if (nextProps.name) {
+				dialogReferenceManager.register(nextProps.name, this);
+			}
+		}
 	}
 
 	_pop(args) {
