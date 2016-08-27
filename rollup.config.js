cbed2c0605f481a9eb11db7878a565cb19cd7b5b
Victoria French <victoria.french@cinecove.com>
2016-08-27 15:39:48 -0500

    file separation, eslint, babel + rollup transpiling

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,20 @@
 import babel from 'rollup-plugin-babel';
-
+import babelrc from 'babelrc-rollup';
 let pkg = require('./package.json');
 
 export default {
   entry: 'lib/index.js',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
   external: Object.keys(pkg.dependencies),
   targets: [
     {
       dest: pkg['main'],
       format: 'es',
+      exports: 'named',
       sourceMaps: false
     }
   ]
